c04cdfb643bc71efe881dc1ea90d3f7a634e0e50
thiagomeloo <4thiagomelo5@gmail.com>
2020-12-20 11:08:04 -0300

    teste deploy

--- src/database/knexfile.js
+++ src/database/knexfile.js
@@ -1,15 +1,16 @@
 // Update with your config settings.
 require('dotenv').config()
-
+console.dir(process.env)
 module.exports = {
 
   production: {
-    client: 'mysql',
+    client: 'pg',
     connection: {
       host: process.env.DB_HOST,
       user: process.env.DB_USER,
       password: process.env.DB_PASSOWRD,
-      database: process.env.DB_DATABASE
+      database: process.env.DB_DATABASE,
+      port:process.env.DB_PORT
     },
     migrations: {
       tableName: 'migrations'
